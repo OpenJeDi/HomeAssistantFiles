088e1500041d8f742e45a65ff6fe0e7948affe8e
Jeroen Dierckx <jeroen.dierckx@outoftheboxproductions.be>
2021-01-05 19:43:43 +0100

    Updated test code

--- custom_components/dobiss/test.py
+++ custom_components/dobiss/test.py
@@ -1,5 +1,12 @@
 import dobiss
+import time
 
-d = dobiss.DobissSystem("192.168.1.116", 10001)
+d = dobiss.DobissSystem("192.168.1.118", 10001)
 d.importFullInstallation()
 d.requestAllStatus()
+
+while True:
+    time.sleep(1)
+    d.requestAllStatus()
+    print(time.time())
+    print(d.values)
