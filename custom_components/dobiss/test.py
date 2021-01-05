98d86fc0c53cd13a7c5f93e8b6b9efa06b6c0dd4
Jeroen Dierckx <jeroen.dierckx@outoftheboxproductions.be>
2021-01-05 21:42:41 +0100

    Updated test script so we can provide an IP address and port

--- custom_components/dobiss/test.py
+++ custom_components/dobiss/test.py
@@ -1,7 +1,21 @@
 import dobiss
 import time
+import sys
 
-d = dobiss.DobissSystem("192.168.1.118", 10001)
+DEFAULT_IP = "192.168.1.118"
+DEFAULT_PORT = 10001
+
+# IP address of the installation
+ip = DEFAULT_IP
+if len(sys.argv) > 1:
+    ip = sys.argv[1]
+
+# TCP port
+port = DEFAULT_PORT
+if len(sys.argv) > 2:
+    port = sys.argv[2]
+
+d = dobiss.DobissSystem(ip, port)
 d.importFullInstallation()
 d.requestAllStatus()
 
